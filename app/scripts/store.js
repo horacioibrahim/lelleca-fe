b20425f07fdef615ccf8f29572c9d562917f75f7
Horacio Ibrahim <horacioibrahim@gmail.com>
2014-12-01 21:53:32 -0200

    #13 Pagina do produto. Melhorando issue com thumbnails/slide

--- app/scripts/store.js
+++ app/scripts/store.js
@@ -4,18 +4,53 @@ var app = angular.module("store", ['angularUtils.directives.dirPagination']);
 app.controller("storeController", function($scope) {
 	$scope.navBarMenus = menus;
 	$scope.catalog = products;
+	$scope.productShow = productShow;
+	$scope.listStars = listStars;
+	$scope.setImage = setImage;
 });
 
 app.config(function(paginationTemplateProvider) {
-    paginationTemplateProvider.setPath('../bower_components/angular-utils-pagination/dirPagination.tpl.html');
+    paginationTemplateProvider
+    	.setPath('../bower_components/angular-utils-pagination/dirPagination.tpl.html');
 });
 
 /* function for Controller */
-function CatalogControllerSearch($scope) {
-	$scope.currentPage = 1;
-	$scope.pageSize = 10;
-	$scope.products = [];
-}
+
+function productShow(pid) {
+	$("#product-modal-lg-" + pid).modal();
+};
+
+function listStars(stars) {
+	// returns class complement for starts from customer Review
+	var default_size = 5;
+	var lack = default_size - stars;
+	var stars_class = [];
+	for (i = 0; i < stars; ++i) {
+		stars_class.push('active');
+	};
+	for (i = 0; i < lack; ++i) {
+		stars_class.push('empty');
+	};	
+
+	//alert(stars_class);
+	return stars_class;
+};
+
+function setImage(pid, imageUrl) {
+	// disable removing .active
+	$('.product-thumb-selected').each(function() {
+		$(this).removeClass('active');
+	});
+	// active to compare mainImg with img.full in the template
+	this.mainImg = imageUrl;
+
+	var elemImgMain = $('#productIDforImg-' + pid);
+	elemImgMain
+		.fadeOut('fast')
+		.fadeIn('fast', function(){
+			elemImgMain.attr('src', imageUrl);		
+		});
+};
 
 /* Data source */
 
@@ -23,123 +58,510 @@ var menus = ['Brinquedos', 'Livros', 'Roupas', 'Calçados', 'Enxoval',
 			'Tutoria & Cursos', 'Moldes'];
 var products = [
 	{
-		'name': 'Flor do Cerrado',
-	 	'short_desc': 'Criada com a força da mulher do cerrado e cor busca.',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 1,
+		'title': 'Boneca Lelleca Flôr do Cerrado',
+		'title_unicode': 'Flor do Cerrado',
+	 	'short_desc': 'Antes de saber que Paul McCartney iria',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',
+		'images': [],
 		'color:': 'vermelho',
-	 	'likes': 25
+	 	'likes': 25,
+	 	'price': 45.91,
+	 	'costFree': false,
+	 	'stars': 3,
+	 	'freeShipping': true,
+	 	'customer_commit': 2,
 	},
 	{
-		'name': 'Ipe Amarelo',
-		'short_desc': 'A cor intensa do Flamboyant que resiste as mais s.',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 2,
+		'title': 'Ipê Amarelo',
+		'short_desc': 'Então minha tia chegou com a notícia',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
 		'color:': ['azul'],
-		'likes': 30
+		'likes': 30,
+		'price': 32.45,
+		'costFree': false,
+		'stars': 4,
+		'freeShipping': false,
+		'customer_commit': 3,
+	},
+	{	
+		'pid': 3,
+		'title': 'Flamboyant',
+		'short_desc': 'Aí tive a ideia de botar a mão na massa',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
+		'color:': ['verde'],
+		'likes': 11,
+		'price': 45.00,
+		'costFree': false,
+		'stars': 3,
+		'freeShipping': false,
+		'customer_commit': 3,
 	},
 	{
-		'name': 'Flamboyant',
-		'short_desc': 'Representa a força do Ipe que tem seu melhor momento ao.',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 4,
+		'title': 'Maça Verde',
+		'short_desc': 'Deu certo. Ele arrecadou em torno de R$ 800',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
 		'color:': ['verde'],
-		'likes': 11
-	},
-	{
-		'name': 'Maca Verde',
-		'short_desc': 'A maca verde eh a fruta de cor verde.',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
-		'color:': ['verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'costFree': false,
+		'stars': 5,
+		'freeShipping': false,
+		'customer_commit': 9,
 	},	
 	{
-		'name': 'Uva Passa',
+		'pid': 5,
+		'title': 'Uva Passa',
 		'short_desc': 'Tudo passa ate uva passa',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},										
+		],
 		'color:': ['Preta'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 2,
+		'freeShipping': false,
+		'customer_commit': 1,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 6,
+		'title': 'Melancia Garota',
+		'short_desc': 'Desde de Carminda Miranda para aparecer basta uma fruta',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},										
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 0,
+		'freeShipping': false,
+		'customer_commit': 4,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 7,
+		'title': 'Manga Rosa',
+		'short_desc': 'As pulseiras de plástico foram vendidas por R$ 4 cada',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 0,
+		'freeShipping': false,
+		'customer_commit': 3,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 8,
+		'title': 'Pessego Garota',
+		'short_desc': 'Uma pessoa que queria nos dar os dois convites ',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},										
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 5,
+		'freeShipping': false,
+		'customer_commit': 2,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 9,
+		'title': 'Bruta Garota',
+		'short_desc': 'Durante a semana, por causa das provas e lições',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 5,
+		'freeShipping': false,
+		'customer_commit': 3,
 	},	
 	{
-		'name': 'Melancia Garota',
+		'pid': 10,
+		'title': 'Melancia Garota',
 		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},									
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 5,
+		'freeShipping': false,
+		'customer_commit': 4,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 11,
+		'title': 'Eita Gota',
+		'short_desc': 'A produção acontece no final de semana',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},										
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 2,
+		'freeShipping': false,
+		'customer_commit': 2,
 	},	
 	{
-		'name': 'Melancia Garota',
-		'short_desc': 'Desde de Carmem Miranda para aparecer basta uma fruta',
-		'images': {
-			'full': 'Inexistent this moment...',
-			'thumb': '../images/sample/300x300.gif'
-		},
+		'pid': 12,
+		'title': 'Melancia Garota',
+		'short_desc': 'O show de McCartney será a primeira atração',
+	 	'description': 'Vestido branco de princesa com detalhes de rosas \
+	 	 bordadas. Produzido com renda renascença. Bordado em pérolas. Renda \
+	 	 clássica. Forro de algodão com manguinha. Saia de organza branca \
+	 	 plissada. Tule por baixo. Nossos produtos são produzidos à mão, \
+	 	 como se fossem feito especialmente para cada criança, com material \
+	 	 de excelente qualidade e ótimo acabamento.',		
+		'images': [
+			{
+			    'full': '../images/sample/300x300.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_1.gif',
+			    'thumb': '../images/sample/thumb_75x75_1.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_2.gif',
+			    'thumb': '../images/sample/thumb_75x75_2.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_3.gif',
+			    'thumb': '../images/sample/thumb_75x75_3.gif'
+
+			},
+			{
+			    'full': '../images/sample/300x300_4.gif',
+			    'thumb': '../images/sample/thumb_75x75_4.gif'
+
+			},										
+		],
 		'color:': ['Vermelho', 'Verde'],
-		'likes': 3
+		'likes': 3,
+		'price': 45.00,
+		'stars': 5,
+		'freeShipping': false,
+		'customer_commit': 10,
 	},
 	];
